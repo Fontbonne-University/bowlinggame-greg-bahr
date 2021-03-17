5d87adb846cb90829eb7390304a4cacbb0713300
greg-bahr <gregsbahr@gmail.com>
2021-03-16 19:34:16 -0500

    first few tests

--- BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
+++ BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
@@ -1,21 +1,67 @@
 package BowlingGame;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BowlingTest {
 
-	@Before
-	public void setUp() throws Exception {
+    private Game g;
 
-	}
+    @Before
+    public void setUp() throws Exception {
+        g = new Game();
+    }
 
-	@Test
-	public void canCreateGame()
-	{
-		Game g = new Game();
-	}
+    private void rollMany(int rolls, int pinsHit) {
+        for (int i = 0; i < rolls; i++) {
+            g.roll(pinsHit);
+        }
+    }
+
+    @Test
+    public void canRoll() {
+        g.roll(0);
+    }
+
+    @Test
+    public void gutterGame() {
+        rollMany(20, 0);
+
+        assertEquals(0, g.score());
+    }
+
+    @Test
+    public void allOnes() {
+        rollMany(20, 1);
+
+        assertEquals(20, g.score());
+    }
+
+    @Test
+    public void oneSpare() {
+        g.roll(5);
+        g.roll(5);
+        g.roll(3);
+        rollMany(17, 0);
+
+        assertEquals(16, g.score());
+    }
+
+    @Test
+    public void oneStrike() {
+        g.roll(10);
+        g.roll(3);
+        g.roll(4);
+        rollMany(16, 0);
+
+        assertEquals(24, g.score());
+    }
+
+    @Test
+    public void perfectGame() {
+        rollMany(12, 10);
+        assertEquals(300, g.score());
+    }
 }
--- BowlingGame/Bowling/src/BowlingGame/Game.java
+++ BowlingGame/Bowling/src/BowlingGame/Game.java
@@ -2,5 +2,53 @@ package BowlingGame;
 
 public class Game {
 
+    private int[] rolls;
+    private int currentRoll;
 
+    public Game() {
+        rolls = new int[21];
+    }
+
+    public void roll(int pinsHit) {
+        rolls[currentRoll] = pinsHit;
+        currentRoll++;
+
+        if (pinsHit == 10 && currentRoll < 19) {
+            rolls[currentRoll] = 0;
+            currentRoll++;
+        }
+    }
+
+    public int score() {
+        int score = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            int firstRoll = rolls[frame*2];
+            int secondRoll = rolls[(frame*2)+1];
+
+            if (isStrike(frame-1)) {
+                if (isStrike(frame)) {
+                    score += 10 + rolls[(frame + 1) * 2];
+                    if (frame == 9) {
+                        score += 10;
+                    }
+                } else {
+                    score += firstRoll + secondRoll;
+                }
+            } else if (isSpare(frame-1)) {
+                score += firstRoll;
+            }
+
+            score += firstRoll + secondRoll;
+        }
+
+        return score;
+    }
+
+    private boolean isStrike(int frame) {
+        return frame >= 0 && rolls[frame * 2] == 10;
+    }
+
+    private boolean isSpare(int frame) {
+        return frame >= 0 && rolls[frame * 2] + rolls[(frame * 2) + 1] == 10;
+    }
 }
